ecd1f3031fc4a7bfd136bc276c0f5a62c7005f8e
Wendell Piez <wendell.piez@nist.gov>
2022-09-20 16:23:26 -0400

    Ouch! had to restructure for Pages publishing

---
+++ lib/xslt-blender.d.ts
@@ -0,0 +1,11 @@
+declare function spliceIntoPage(targetID: string, doc: Document, clearing: Boolean): void;
+declare function xsltEngineForHREF(xsltHREF: string): Promise<XSLTProcessor>;
+declare function parseXMLLiteral(xmlLiteral: string): Document;
+declare function parseAndTransformXMLLiteral(xmlLiteral: string, xsltEngine: XSLTProcessor): Document;
+declare function parseAndTransformXMLatHREF(httpResource: string, xsltEngine: XSLTProcessor): Promise<Document>;
+declare function xslt1ResultDocument(xmlDom: Document, xsltEngine: XSLTProcessor): Document;
+declare function getXMLviaHTTP(httpResource: string): Promise<Document>;
+declare function writeOBJtoXML(obj: any): string;
+declare function fileXMLContent(fileToRead: File): Promise<Document>;
+declare function fileTextContent(fileToRead: File): Promise<string>;
+export { fileTextContent, fileXMLContent, getXMLviaHTTP, writeOBJtoXML, spliceIntoPage, parseXMLLiteral, parseAndTransformXMLLiteral, parseAndTransformXMLatHREF, xslt1ResultDocument, xsltEngineForHREF };
