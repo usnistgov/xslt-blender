36fa6651918b5ebf84dccb446829173df3d68d26
Wendell Piez <wendell.piez@nist.gov>
2022-09-29 12:10:31 -0400

    XSLT Blender extensions for reading HTML (not demonstrably working yet)

--- lib/xslt-blender.d.ts
+++ lib/xslt-blender.d.ts
@@ -7,5 +7,6 @@ declare function xslt1ResultDocument(xmlDom: Document, xsltEngine: XSLTProcessor
 declare function getXMLviaHTTP(httpResource: string): Promise<Document>;
 declare function writeOBJtoXML(obj: any): string;
 declare function fileXMLContent(fileToRead: File): Promise<Document>;
+declare function fileHTMLContent(fileToRead: File): Promise<Document>;
 declare function fileTextContent(fileToRead: File): Promise<string>;
-export { fileTextContent, fileXMLContent, getXMLviaHTTP, writeOBJtoXML, spliceIntoPage, parseXMLLiteral, parseAndTransformXMLLiteral, parseAndTransformXMLatHREF, xslt1ResultDocument, xsltEngineForHREF };
+export { fileTextContent, fileXMLContent, fileHTMLContent, getXMLviaHTTP, writeOBJtoXML, spliceIntoPage, parseXMLLiteral, parseAndTransformXMLLiteral, parseAndTransformXMLatHREF, xslt1ResultDocument, xsltEngineForHREF };
