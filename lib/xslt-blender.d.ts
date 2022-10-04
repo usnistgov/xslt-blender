17f6304db34c2df03a84356d1d9551f27f354fb8
Wendell Piez <wendell.piez@nist.gov>
2022-10-04 08:00:14 -0400

    Building out HTML Reducer

--- lib/xslt-blender.d.ts
+++ lib/xslt-blender.d.ts
@@ -1,3 +1,4 @@
+declare const clearElementById: (targetID: string) => void;
 declare function spliceIntoPage(targetID: string, doc: Document, clearing: Boolean): void;
 declare function xsltEngineForHREF(xsltHREF: string): Promise<XSLTProcessor>;
 declare function parseXMLLiteral(xmlLiteral: string): Document;
@@ -9,4 +10,4 @@ declare function writeOBJtoXML(obj: any): string;
 declare function fileXMLContent(fileToRead: File): Promise<Document>;
 declare function fileHTMLContent(fileToRead: File): Promise<Document>;
 declare function fileTextContent(fileToRead: File): Promise<string>;
-export { fileTextContent, fileXMLContent, fileHTMLContent, getXMLviaHTTP, writeOBJtoXML, spliceIntoPage, parseXMLLiteral, parseAndTransformXMLLiteral, parseAndTransformXMLatHREF, xslt1ResultDocument, xsltEngineForHREF };
+export { fileTextContent, fileXMLContent, fileHTMLContent, getXMLviaHTTP, writeOBJtoXML, clearElementById, spliceIntoPage, parseXMLLiteral, parseAndTransformXMLLiteral, parseAndTransformXMLatHREF, xslt1ResultDocument, xsltEngineForHREF };
